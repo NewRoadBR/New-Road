e80920392c4f8acd2bba52212a8c554b3ce6597f
Gustavo982-web <gustavo.ssantos@sptech.school>
2026-04-24 12:46:45 -0300

    login conectado com banco contarizado

--- web-data-viz-main/src/controllers/usuarioController.js
+++ web-data-viz-main/src/controllers/usuarioController.js
@@ -1,55 +1,50 @@
-var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+const usuarioModel = require("../models/usuarioModel");
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
 
-    if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está indefinida!");
-    } else {
-
-        usuarioModel.autenticar(email, senha)
-            .then(
-                function (resultadoAutenticar) {
-                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
-
-                    if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
-
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
-                    } else if (resultadoAutenticar.length == 0) {
-                        res.status(403).send("Email e/ou senha inválido(s)");
-                    } else {
-                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-                    }
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
+    if (!email) {
+        return res.status(400).send("Email não informado");
     }
 
+    if (!senha) {
+        return res.status(400).send("Senha não informada");
+    }
+
+    usuarioModel.autenticar(email, senha)
+        .then(function (resultado) {
+
+            console.log("Resultado login:", resultado);
+
+            if (resultado.length === 0) {
+                return res.status(403).send("Email ou senha inválidos");
+            }
+
+            if (resultado.length > 1) {
+                return res.status(403).send("Erro: múltiplos usuários com mesmo login");
+            }
+
+            var usuario = resultado[0];
+
+            return res.status(200).json({
+                id: usuario.id,
+                nome: usuario.nome,
+                email: usuario.email,
+                empresaId: usuario.empresaId
+            });
+
+        })
+        .catch(function (erro) {
+            console.error("Erro no login:", erro);
+
+            return res.status(500).json({
+                erro: "Erro interno no servidor",
+                detalhe: erro.sqlMessage || erro.message
+            });
+        });
 }
+
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
