2aba392b3f3319deb2167b60694bb3dfda4c27d1
Gustavo982-web <gustavo.ssantos@sptech.school>
2026-04-24 13:27:52 -0300

    Funciona rota

--- web-data-viz-main/src/models/usuarioModel.js
+++ web-data-viz-main/src/models/usuarioModel.js
@@ -1,12 +1,17 @@
 var database = require("../database/config")
 
 function autenticar(email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    console.log("Autenticando:", email);
+
     var instrucaoSql = `
-        SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT id, nome, email, fk_empresa as empresaId
+        FROM usuario
+        WHERE email = ? AND senha = ?;
     `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+
+    console.log("Executando SQL com parâmetros");
+
+    return database.executar(instrucaoSql, [email, senha]);
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
