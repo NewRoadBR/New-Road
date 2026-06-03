550f2d616398d6459ebe3c96f1f52e41b67dff2c
Gustavo982-web <gustavo.ssantos@sptech.school>
2026-06-03 00:31:33 -0300

    atualizando entry

--- etl-v3/entrypoint.sh
+++ etl-v3/entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 set -e
-cat > /app/application.properties << PROPS
-etl.fonte=${ETL_FONTE:-s3}
-s3.bucket=${S3_BUCKET}
-s3.prefixo=${S3_PREFIXO:-}
-s3.regiao=${S3_REGIAO:-us-east-1}
-db.url=jdbc:mysql://${DB_HOST:-mysql-container}:${DB_PORT:-3306}/${DB_NAME:-newroad_db}?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true&allowLoadLocalInfile=true
+
+# Gerando o arquivo database.properties (que seu código Java usa)
+cat > /app/database.properties << PROPS
+db.url=jdbc:mysql://${DB_HOST:-mysql-container}:${DB_PORT:-3306}/${DB_NAME:-newroad_db}?useSSL=false&serverTimezone=America/Sao_Paulo&allowPublicKeyRetrieval=true&rewriteBatchedStatements=true&allowLoadLocalInfile=true
 db.usuario=${DB_USER:-root}
 db.senha=${DB_PASSWORD}
-etl.pasta.temp=/app/temp_csv
-etl.min.medicoes=${ETL_MIN_MEDICOES:-5}
+
+etl.tamanho.lote=5000
+etl.fonte=${ETL_FONTE:-S3}
+etl.s3.bucket=${S3_BUCKET}
+etl.s3.prefixo=${S3_PREFIXO:-}
+etl.s3.regiao=${S3_REGIAO:-us-east-1}
 PROPS
-exec java -jar app.jar
+
+# Dizendo para o Java rodar procurando as configurações no diretório atual primeiro
+exec java -cp /app:/app/app.jar br.com.newroad.Main
